317c784df3c6b3c1b9b01cd240b2d37f08872378
redhat-debug <https://github.com/redhat-debug/ishq.git>
2022-06-10 11:09:45 +0600

    revision done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,22 +7,16 @@
         // when window width is >= 320px
         320: {
           slidesPerView: 1,
-          spaceBetween: 40
+          spaceBetween: 30
         },
-        // when window width is >= 992px
-        575: {
+
+        768: {
           slidesPerView: 2,
-          spaceBetween: 40
+          spaceBetween: 30
         },
-        // when window width is >= 1400px
-        850: {
+        991: {
           slidesPerView: 3,
-          spaceBetween: 50
-        },
-        // 
-        1200: {
-          slidesPerView: 4,
-          spaceBetween: 20
+          spaceBetween: 30
         }
       }
     });
